079cee52ccaa94ef169953455ff4ae5cd7dc3f9a
Pattabhi A <pattabhi.amperayani@gmail.com>
2025-04-19 10:00:00 +0530

    adding welcome component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Greet from './components/Greet';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Greet from "./components/Greet";
+import EventBind from "./components/EventBind";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Greet />
+        <Greet name="Bruce" messageCount={20} isLoggedIn={true} />
+        <EventBind />
       </div>
-    )
+    );
   }
 }
 
